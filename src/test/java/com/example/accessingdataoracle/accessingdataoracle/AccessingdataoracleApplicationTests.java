624848d5ba07f077b6e6ea2106ed8c6d4fedc50a
shinji8822 <shinji0125+github@gmail.com>
2023-02-18 03:25:33 +0000

    first commit

---
+++ src/test/java/com/example/accessingdataoracle/accessingdataoracle/AccessingdataoracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingdataoracle.accessingdataoracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingdataoracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
